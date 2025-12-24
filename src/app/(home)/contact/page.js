525b11deff84fb32dfaa6bec5d8299191c585a33
Daniel <davidtea0427@gmail.com>
2025-12-24 10:18:35 +0800

    feat/contact

--- src/app/(home)/contact/page.js
+++ src/app/(home)/contact/page.js
@@ -1,5 +1,8 @@
 import Csr from "./Csr";
 import { createClient } from '@/lib/supabase-server';
+import worker from '@/shared/image/worker.webp';
+import Image from "next/image";
+import styles from './contact.module.scss';
 
 const res = await fetch('https://randomuser.me/api/');
 const { results } = await res.json();
@@ -18,6 +21,45 @@ async function About() {
     }
 
     return (<>
+        <div className="mt-13">
+            <div className="container">
+                <div className="row">
+                    <div className="col-7">
+                        <p>What we do</p>
+                        <p className="fs-2 my-2">We always make the best</p>
+                        <p className="mb-8">Founded in 1981 in New Taipei City, Taiwan, Fotado Enterprise has evolved into a globally recognized supplier of premium automotive aftermarket parts. With more than 40 years of dedication to quality and innovation, we proudly serve customers on five continents.</p>
+                        <button type="button" className="btn btn-neutral-80 d-block"><i className="bi bi-envelope-fill"></i> Contact us</button>
+                    </div>
+                    <div className="col-5">
+                        <div>
+                            <Image
+                                src={worker}
+                                width={331}
+                                height={331}
+                                className={`img-base ${styles.imgGrayscale}`}
+
+                                alt=""
+
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className={styles.certification}>
+            <div className={styles['certification-container']}>
+                <div className={styles['certification-content']}>
+                    <div className={styles['certification-image']}>
+                        <Image src={worker} width={331} height={331} alt="" className="img-base" />
+                    </div>
+                    <div className={styles['certification-text']}>
+                        <p>For over four decades, Fotado Enterprise has remained steadfast in our philosophy of “Quality First, Customer Priority.” Through continuous innovation, superior product quality, and responsive service, we are committed to being your most dependable long-term partner in the global automotive aftermarket.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <h2>This is about page e</h2>
         <p>{data.name.first}</p>
         <img src={data.picture.large} alt="" />
